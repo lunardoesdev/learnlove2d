bd68bbf89943b0ef21ab3c4b844c01eb21ae9619
Lunar <lunarlifeburst@gmail.com>
2026-09-01 13:32:35 +1000

    square to take 20% of lower space

--- plinko/main.lua
+++ plinko/main.lua
@@ -3,9 +3,16 @@ function love.load()
 end
 
 function love.update(dt)
-  
+
 end
 
 function love.draw()
-  love.graphics.print("Hello, Love2d", 300, 300)
+    local w = love.graphics.getWidth()
+    local h = love.graphics.getHeight()
+
+    local rectH = h * 0.2
+    love.graphics.setColor(0.5, 0.5, 0.5)
+    love.graphics.rectangle("fill", 0, h - rectH, w, rectH)
+
+    love.graphics.print("Hello, Love2d", 300, 300)
 end
